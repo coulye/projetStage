76bafbd0bc63aec0f64254f647ddc4615f1da303
coulye <coolye@hotmail.fr>
2018-06-11 11:28:11 +0200

    projet de stage

--- beans/inserter.js
+++ beans/inserter.js
@@ -96,37 +96,51 @@ exports.inserter = {
         var dataIntoArray = [];
         var k = 0;
         // preparation des données reçues
+        /** todo : Attention ici on doit parcourir les 24 lignes de saisie et pas seulement les fields du body */
         for (var field in dataArray) {
             if (!Array.isArray(dataArray[field])) {
                 dataMiseajour[field] = dataArray[field];
             }
-            else {
-                /** todo : ici transformer le tableau pour avoir un tableau de data par collection */
-                dataIntoArray.push({
-                    "libelle": dataArray["libelle"][k],
-                    "tel": dataArray["tel"][k],
-                    "adresse1": dataArray["adresse1"][k],
-                    "adresse2": dataArray["adresse2"][k],
-                    "code_postal": dataArray["code_postal"][k],
-                    "ville": dataArray["ville"][k]
-                });   // ici field est un Array
-                k++;
-            }
         }
+        var nbFieldsToinsert = dataArray["libelle"].length;
+        for (var k = 0; k < nbFieldsToinsert; k++) {
+            /** todo : ici transformer le tableau pour avoir un tableau de data par collection */
+            dataIntoArray.push({
+                "libelle": dataArray["libelle"][k],
+                "tel": dataArray["tel"][k],
+                "adresse1": dataArray["adresse1"][k],
+                "adresse2": dataArray["adresse2"][k],
+                "code_postal": dataArray["code_postal"][k],
+                "ville": dataArray["ville"][k]
+            });
+        }
+        // insertion des données de la collection MiseAJour, entête du formulaire
         GLOBAL.schemas[model[0]].createDocument(dataMiseajour, function (err, data_inserted1) {
             var tabIds = [];
-            logger.debug("objet inséré via inserter.list : ", data_inserted1);
+            logger.debug("objet inséré via inserter.registreEtape : ", data_inserted1);
 
-            function insertArray(i, cbk) {
+            // fonction récursive pour insérer les numéro de téléphone des différentes organisation et catgorie
+            function insertArray(j, cbk) {
                 logger.debug(" List inserter call");
+                var numeroModel = 0;
                 //sio.sockets.in(_controler.room).emit('user', {room: _controler.room, comment: ' One User\n\t Your Filter is :'});
                 try {
-                    if (i < dataIntoArray.length) {
-                        GLOBAL.schemas[model[i]].createDocument(dataIntoArray[i], function (err, data_inserted) {
-                            //tabIds[model[i]] = [];
-                            logger.debug("objet inséré via inserter.list : ", data_inserted);
+                    if (j < 24) {
+                        if (j < 14) {
+                            numeroModel = 2;
+                        }
+                        else if (j > 13 && j < 20) {
+                            numeroModel = 3;
+                        }
+                        else if (j > 19 && j < 24) {
+                            numeroModel = 4;
+                        }
+                        GLOBAL.schemas[model[numeroModel]].createDocument(dataIntoArray[j], function (err, data_inserted) {
+                            logger.debug("objet inséré via inserter.registreEtape : ", data_inserted);
+                            /** todo : trouver une meilleurs façon de stoàcker les ids des ligne insérer denuméros de tél
+                             * todo : pour les insérer des la collection NumeroUtiles                                     */
                             tabIds.push(data_inserted._id);
-                            insertArray(i + 1, cbk);
+                            insertArray(j + 1, cbk);
                         });
                     } else {
                         cbk();
@@ -134,18 +148,27 @@ exports.inserter = {
                 } catch (err) { // si existe pas alors exception et on l'intègre via mongooseGeneric
                     return cb(err);
                 }
-
             }
-
+            /** todo : il faut plusieurs appel recursif pour traiter l'ensemble des champs de chaque catégorie */
             insertArray(0, function () {
                 logger.debug('documents insérés');
-                GLOBAL.schemas[model[1]].createDocument(dataIntoArray[1], function (err, data_inserted2) {
+                /** todo ici on va gérer les ids des numéros de tél des différentes organisations our l'insérer dans NumeroUtile*/
+                console.log('dataIntoArray : ', dataIntoArray);
+                console.log('tabIds : ', tabIds);
+                //return cb(null, {data: dataIntoArray, room: _controler.room});
+                /** todo : ici on doit découper le tableau des ids précédemment insérés pour les ajouter à la collection NumeroUtiles */
+                    //préparation des données sous la forme de 3 tableaux d'_ids
+                var dataToInsert = {
+                        installateurs_extId : tabIds.slice(14, 20),
+                        verificateursAgrees_extId: tabIds.slice(20),
+                        numerosUrgence_extId: tabIds.slice(0, 14)
+                    }
+                console.log('les numéros utiles a insérer : ', dataToInsert);
+                GLOBAL.schemas[model[1]].createDocument(dataToInsert, function (err, data_inserted2) {
                     logger.debug("data insérées : ", data_inserted2);
-                    return cb(null, {data: nbInserted, room: _controler.room});
-                })
-
+                    return cb(null, {data: data_inserted2, room: _controler.room});
+                });
             });
-
         });
     },
 
